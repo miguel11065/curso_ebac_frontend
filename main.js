a20d9fb9a5e57188d76049ada6cc5098074c3879
miguel11065 <126350222+miguel11065@users.noreply.github.com>
2023-08-04 15:13:29 -0300

    corrigindo erros e adicionando classes herdeiras

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-function Filme(nome) {
-    this.nome = nome;
-    this.plataforma1 = function() {
-        console.log(`- O filme ${this.nome} está disponivel no Prime Video!`)
-    }
-}
-
-function Serie(nome) {
-    this.nome = nome;
-    this.plataforma2 = function() {
-        console.log(`- A série ${this.nome} está disponivel na Netflix!`)
-    }
-}
-
-function Programa(nome, lancamento, duracao, tipo) {
+function Filme(nome, lancamento, duracao) {
     this.nome = nome;
     this.lancamento = lancamento;
     this.duracao = duracao;
-
-    console.log(`${this.nome} foi lançado em ${this.lancamento} e tem ${this.duracao}.`)
-
-    if(tipo === "Filme") {
-        let filme = new Filme(nome);
-        filme.plataforma1();
-    } else if(tipo === "Série") {
-        let serie = new Serie(nome);
-        serie.plataforma2();
-    } else {
-        console.log("O programa não esta disponivel em nenhum servico de streaming.")
+    this.dizInfo = function() {
+        console.log(` - ${this.nome} já vai começar, aproveite!`);
     }
-
-    Filme.call(this, nome);
-    Serie.call(this, nome);
 }
 
-const filme1 = new Programa("John Wick", "2014", "1h41m", "Filme");
-const serie1 = new Programa("The Witcher", "2019", "3 temporadas", "Série");
+function Acao(nome, lancamento, duracao, genero) {
+    this.genero = genero;
+    
+    Filme.call(this, nome, lancamento, duracao)
+    
+    console.log(`${this.nome} foi um filme de ${this.genero} lançado em ${this.lancamento} e tem ${this.duracao}.`)
+}
+
+function Comedia(nome, lancamento, duracao, genero) {
+    this.genero = genero;
+    
+    Filme.call(this, nome, lancamento, duracao)
+    
+    console.log(`${this.nome} foi um filme de ${this.genero} lançado em ${this.lancamento} e tem ${this.duracao}.`)
+}
+
+const acao1 = new Acao("John Wick", "2014", "1h41m", "ação");
+acao1.dizInfo();
+const comedia1 = new Comedia("Familia do bagulho", "2013", "1h50m", "comédia");
+comedia1.dizInfo();
+const acao2 = new Acao("O resgate", "2020", "1h56m", "ação");
+acao1.dizInfo();
+const comedia2 = new Comedia("Minha mãe é uma peça", "2012", "1h24m", "comédia");
+comedia1.dizInfo();
