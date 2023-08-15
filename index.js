6b52d1d236f1d9aeb9e6d1dd9f22c9a5f883a6d9
miguel11065 <126350222+miguel11065@users.noreply.github.com>
2023-08-15 15:53:35 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function dizOla(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(7, 9));
+console.log(dizOla("Miguel"));
---
+++ index.ts
@@ -0,0 +1,10 @@
+function multiplicar( a: number , b: number){
+    return a * b
+}
+
+function dizOla(nome: string){
+    return "Olá " + nome;
+}
+
+console.log(multiplicar(7, 9));
+console.log(dizOla("Miguel"))
